981671beb9c89b6faa639034d0da335450b22495
kikoribeiro <kikovribeiro@gmail.com>
2024-01-08 19:03:06 +0000

    various changes to forms

--- app/conn/login_con.php
+++ app/conn/login_con.php
@@ -5,7 +5,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $password = $_POST['password'];
 
-    // Retrieve user from the database based on username
     $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username");
     $stmt->bindParam(':username', $username, PDO::PARAM_STR);
     $stmt->execute();
@@ -13,7 +12,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
     if ($user && password_verify($password, $user['password_hash'])) {
-        
 
         if ($user['admin'] == 1) {
             
