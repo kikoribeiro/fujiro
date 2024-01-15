e677dcc0abe9fee9dbd5f13efa1bbf9933d45614
kikoribeiro <kikovribeiro@gmail.com>
2024-01-15 21:10:23 +0000

    Update remove_reservation.php

--- app/conn/remove_reservation.php
+++ app/conn/remove_reservation.php
@@ -5,15 +5,13 @@ require 'config.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['remove_reservation'])) {
     $reservation_id = isset($_POST['reservation_id']) ? $_POST['reservation_id'] : null;
     
-
     if (is_numeric($reservation_id)) {
-
-        $stmt = $pdo->prepare("DELETE FROM reservations WHERE id = :reservation_id");
+        $stmt = $pdo->prepare("DELETE FROM reservations WHERE id = :reservation_id AND user_id = :user_id");
         $stmt->bindParam(':reservation_id', $reservation_id, PDO::PARAM_INT);
+        $stmt->bindParam(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
         $stmt->execute();
 
         header("Location: /fujiro/fujiro/app/reservations.php");
         exit();
     }
-
 }
